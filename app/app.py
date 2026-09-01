160eca336f636416d815c9c1948d2a43d2797108
shubham-chitalkar <shubhamchitalkar21@gmail.com>
2026-09-01 10:43:03 +0530

    Update application to version 1.1

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps! Version 1.0"
+    return "Hello DevOps! Version 1.1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
